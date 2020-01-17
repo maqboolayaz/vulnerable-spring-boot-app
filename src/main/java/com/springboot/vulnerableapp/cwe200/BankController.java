5952fa2723ea5175d8a31d3b6bae7b81758d1760
Ayaz Maqbool <ayazmaqbooluet@gmail.com>
2020-01-17 17:28:55 +0100

    esapi encoder as sanitizer for sql injections cwe

--- src/main/java/com/springboot/vulnerableapp/cwe200/BankController.java
+++ src/main/java/com/springboot/vulnerableapp/cwe200/BankController.java
@@ -1,10 +1,16 @@
 package com.springboot.vulnerableapp.cwe200;
 
 import com.springboot.vulnerableapp.hello.Message;
+import org.owasp.esapi.ESAPI;
+import org.owasp.esapi.codecs.MySQLCodec;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -29,14 +35,36 @@ public class BankController {
                 ResultSet queryResult = stmt.executeQuery(query);
                 userAccount = (BankAccount)queryResult.getObject(accountNumber);
             }
-        } catch (SQLException ex) {
+        } catch (Exception ex) {
             String logMessage = "Unable to retrieve account information from database,\nquery: " + query;
             Logger.getLogger(BankAccount.class.getName()).log(Level.SEVERE, logMessage, ex);
         }
         return userAccount;
     }
 
+    @RequestMapping("/getGet")
+    protected void getUserUrl(HttpServletRequest request , HttpServletResponse response) throws ServletException, IOException
+    {
+        try
+        {
+            String userId = request.getParameter("userId");
+            userId = ESAPI.encoder().encodeForSQL(new MySQLCodec(MySQLCodec.Mode.STANDARD), userId);
+            DatabaseManager dbManager = new DatabaseManager();
+            Connection conn = dbManager.getConnection();
+            Statement st = conn.createStatement();
+            String query = "SELECT * FROM User WHERE userId='" + userId + "';";
+            ResultSet res = st.executeQuery(query);
+            String url = "https://" +userId+ ".company.com";
+            response.sendRedirect(url);
+        }
+        catch (Exception e)
+        {
+            Logger.getLogger(e.getMessage());
+        }
+    }
+
     private boolean isAuthorizedUser(String userName) {
+        //for testing purposes
         return true;
     }
 }
